88626185591929c310785ad27fc1da0a6e074f17
Gianggg13121997 <timfb113@gmail.com>
2024-06-22 17:05:29 +0700

    Create produc.js

---
+++ produc.js
@@ -0,0 +1,12 @@
+class Product {
+    id;
+    name;
+    price;
+    img
+    constructor(idInput,nameInput,priceInput,imgInput){
+        this.id = idInput;
+        this.name = nameInput;
+        this.price = priceInput;
+        this.img = imgInput;
+    }
+}
